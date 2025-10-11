8df76749627ad017e96788a3a588e41d845d9cec
Nick Mours <nickxylanmelloria@gmail.com>
2025-10-11 09:19:03 +0800

    project setup on server side

---
+++ server/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+import connectToMongoDB from './database/mongodb';
+import { notFoundHandler } from './middlewares/not-found';
+import { errorHandler } from './middlewares/error';
+import { healthcheck } from './middlewares/healthcheck';
+import { corsOptions } from './utils/cors';
+import { PORT } from './constants/env';
+
+import { auth } from './middlewares/auth';
+// import { limiter } from './utils/rate-limiter';
+import helmet from 'helmet';
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.get('/', healthcheck);
+
+// Routes
+app.use(auth);
+
+// Error handlers
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+	await connectToMongoDB();
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
