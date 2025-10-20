07513304826d70b4cf8ff6a2a1d3376afb21f654
Zoidbalba <2301104667@student.buksu.edu.ph>
2025-10-20 22:33:07 +0800

    signup design

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,9 +1,24 @@
 import SignupForm from '@/components/forms/SignupForm';
+import signupGraphic from '../assets/images/signup_image.jpg'; // Import your signup illustration
+import "../assets/signup.css";
 
 export default function Signup() {
-	return (
-		<main className='min-h-screen w-full flex flex-col justify-center items-center'>
-			<SignupForm />
-		</main>
-	);
+  return (
+    <main className="auth-container">
+      <div className="auth-panel auth-panel-bordered">
+        {/* Left illustration panel */}
+        <div className="illustration-section-with-white">
+          <img
+            src={signupGraphic}
+            alt="Signup Illustration"
+            className="auth-illustration"
+          />
+        </div>
+        {/* Right gradient form panel */}
+        <div className="form-section-with-gradient">
+          <SignupForm />
+        </div>
+      </div>
+    </main>
+  );
 }
