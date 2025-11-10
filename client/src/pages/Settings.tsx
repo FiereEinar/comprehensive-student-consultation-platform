4dce02334053fe3cdb7fec423eb277b8857d3736
Roberunvencht <2301101444@student.buksu.edu.ph>
2025-11-10 22:35:51 +0800

    Nick added the availabilities

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -2,8 +2,15 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Header from '@/components/ui/header';
 import UpdateProfileForm from '@/components/forms/UpdateProfileForm';
 import ResetPasswordForm from '@/components/forms/ResetPasswordForm';
+import InstructorAvailabilities from '@/components/InstructorAvailabilities';
+import UpdateAvailability from '@/components/UpdateAvailability';
+
+import { useUserStore } from '@/stores/user';
 
 export default function Settings() {
+
+  const user = useUserStore((state) => state.user);
+
 	return (
 		<div className='max-w-2xl space-y-5'>
 			<div>
@@ -29,6 +36,27 @@ export default function Settings() {
 					<ResetPasswordForm />
 				</CardContent>
 			</Card>
+			<Card>
+				<CardHeader>
+				<CardTitle>Set Your Availability</CardTitle>
+				</CardHeader>
+				<CardContent>
+				{user && user.role === 'instructor' ? (
+					<>
+					{/* Form for instructor to set availability */}
+					<UpdateAvailability user={user} />
+
+					{/* Divider or spacing */}
+					<hr className="my-6" />
+
+					{/* Show the instructor's current availabilities */}
+					<InstructorAvailabilities instructorID={user._id} />
+					</>
+				) : (
+					<p>You must be an instructor to set or view availabilities.</p>
+				)}
+				</CardContent>
+      		</Card>
 		</div>
 	);
 }
