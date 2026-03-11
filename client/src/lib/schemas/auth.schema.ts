aa4c04d6ca9dbe0a01614aac1243911e56241fe3
Nick Mours <nickxylanmelloria@gmail.com>
2026-03-11 20:46:42 +0800

    merged bugfixes

--- client/src/lib/schemas/auth.schema.ts
+++ client/src/lib/schemas/auth.schema.ts
@@ -1,14 +1,23 @@
 import z from 'zod';
 
-const MIN_PASSWORD_LEN = 8;
+// const MIN_PASSWORD_LEN = 8;
 
 const passwordValidation = z
 	.string()
 	.min(8, 'Password must be at least 8 characters')
-	.refine((val) => /[A-Z]/.test(val), 'Password must contain at least 1 uppercase letter')
-	.refine((val) => /[a-z]/.test(val), 'Password must contain at least 1 lowercase letter')
+	.refine(
+		(val) => /[A-Z]/.test(val),
+		'Password must contain at least 1 uppercase letter',
+	)
+	.refine(
+		(val) => /[a-z]/.test(val),
+		'Password must contain at least 1 lowercase letter',
+	)
 	.refine((val) => /[0-9]/.test(val), 'Password must contain at least 1 number')
-	.refine((val) => /[!@#$%^&*()\-_=+\[\]{};:'",.<>/?]/.test(val), 'Password must contain at least 1 special character');
+	.refine(
+		(val) => /[!@#$%^&*()\-_=+\[\]{};:'",.<>/?]/.test(val),
+		'Password must contain at least 1 special character',
+	);
 
 export const signupSchema = z
 	.object({
@@ -16,7 +25,9 @@ export const signupSchema = z
 		institutionalID: z.string().min(1, 'Institutional ID is required'),
 		email: z.string().email('Invalid email'),
 		password: passwordValidation,
-		confirmPassword: z.string().min(8, 'Password must be at least 8 characters'),
+		confirmPassword: z
+			.string()
+			.min(8, 'Password must be at least 8 characters'),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		message: 'Passwords do not match',
@@ -32,7 +43,9 @@ export const completeInstructorAccountSchema = z
 	.object({
 		name: z.string().min(1, 'Full name is required'),
 		password: passwordValidation,
-		confirmPassword: z.string().min(8, 'Password must be at least 8 characters'),
+		confirmPassword: z
+			.string()
+			.min(8, 'Password must be at least 8 characters'),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		message: 'Passwords do not match',
